4197e11bdcdc0034b693490d2f89b0f482f3fb7e
Alex Gavrusev <alex@gavrusev.dev>
2024-08-29 11:17:32 +0300

    feat: remove branch from statusline, replace trouble with file path

--- nvim/lua/plugins/statusline.lua
+++ nvim/lua/plugins/statusline.lua
@@ -4,29 +4,18 @@ return {
 		"nvim-tree/nvim-web-devicons"
 	},
 	config = function()
-		local lualine_b = { "branch", "diagnostics" };
-		local lualine_c = {};
+		local lualine_b = {
+			{
+				"filename",
+				path = 1
+			}
+		};
+		local lualine_c = {
+			"diagnostics"
+		};
 		local lualine_x = { "filetype" };
 		local lualine_y = { "progress" };
 		local lualine_z = { "location" };
-
-
-		local trouble = require("trouble")
-		local symbols = trouble.statusline({
-			mode = "lsp_document_symbols",
-			groups = {},
-			title = false,
-			filter = { range = true },
-			format = "{kind_icon}{symbol.name:Normal} {hl:lualine_b_normal}",
-			-- The following line is needed to fix the background color
-			-- Set it to the lualine section you want to use
-			hl_group = "lualine_c_normal",
-			sep = ""
-		})
-		table.insert(lualine_c, {
-			symbols.get,
-			cond = symbols.has,
-		})
 
 		local opts = {
 			sections = {
