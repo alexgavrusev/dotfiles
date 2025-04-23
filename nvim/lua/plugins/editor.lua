61302ee911c9f0345ee97892895b1079c56adf63
Alex Gavrusev <alex@gavrusev.dev>
2025-04-23 14:23:24 +0300

    feat: add split keymaps to mini files

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"echasnovski/mini.files",
+		version = "*",
 		keys = require("config.keymaps").mini_files,
 		opts = {
 			-- Don't use `h`/`l` for easier cursor navigation during text edit
@@ -67,6 +68,37 @@ return {
 				pattern = 'MiniFilesActionDelete',
 				callback = function(args)
 					lsp_utils.on_delete_file(args.data.from)
+				end,
+				group = mini_files_group
+			})
+
+
+			local map_split = function(buf_id, lhs, direction)
+				local rhs = function()
+					-- Make new window and set it as target
+					local cur_target = MiniFiles.get_explorer_state().target_window
+					local new_target = vim.api.nvim_win_call(cur_target, function()
+						vim.cmd(direction .. ' split')
+						return vim.api.nvim_get_current_win()
+					end)
+
+					MiniFiles.set_target_window(new_target)
+					MiniFiles.go_in({ close_on_file = true })
+				end
+
+				-- Adding `desc` will result into `show_help` entries
+				local desc = 'Split ' .. direction
+				vim.keymap.set('n', lhs, rhs, { buffer = buf_id, desc = desc })
+			end
+
+			vim.api.nvim_create_autocmd('User', {
+				pattern = 'MiniFilesBufferCreate',
+				callback = function(args)
+					local buf_id = args.data.buf_id
+					-- Tweak keys to your liking
+					map_split(buf_id, '<C-s>', 'belowright horizontal')
+					map_split(buf_id, '<C-v>', 'belowright vertical')
+					map_split(buf_id, '<C-t>', 'tab')
 				end,
 				group = mini_files_group
 			})
