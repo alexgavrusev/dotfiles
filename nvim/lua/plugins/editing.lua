2b756240b3a2f6a22d156a99809200a1782aca45
Alex Gavrusev <alex@gavrusev.dev>
2025-04-23 11:32:23 +0300

    feat: tweak comments opts

--- nvim/lua/plugins/editing.lua
+++ nvim/lua/plugins/editing.lua
@@ -12,5 +12,21 @@ return {
 		version = "*",
 		event = "InsertEnter",
 		opts = {}
+	},
+	{
+		"numToStr/Comment.nvim",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"JoosepAlviste/nvim-ts-context-commentstring",
+		},
+		event = "VeryLazy",
+		config = function()
+			require("Comment").setup({
+				---Function to call before (un)comment
+				---nvim-ts-context-commentstring is used to add support for tsx/jsx
+				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+			})
+		end,
 	}
+
 }
