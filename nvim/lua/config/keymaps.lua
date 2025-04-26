485e0d8a3eae2990baefc877c7632415c291b6f7
Alex Gavrusev <alex@gavrusev.dev>
2025-04-26 21:19:59 +0300

    feat: remove indent keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -15,13 +15,6 @@ M.init = function()
 	-- clear selection, then clear the command line, then do the actual <CR>
 	map("n", "<Esc>", ":let @/=''<CR>:echo ' '<CR>", { silent = true })
 
-	-- https://www.reddit.com/r/neovim/comments/12pqkjo/comment/jgn0lct
-	map("n", "<", "<<", { silent = true, desc = "Outdent" })
-	map("n", ">", ">>", { silent = true, desc = "Indent" })
-	map("v", "<", "<gv", { silent = true, desc = "Outdent" })
-	map("v", ">", ">gv", { silent = true, desc = "Indent" })
-
-
 	-- picker used for document symbols instead
 	vim.keymap.del("n", "gO")
 end
