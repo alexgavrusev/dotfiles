1479b3dc74df08a9d367e396d2eeca980a56875d
Alex Gavrusev <alex@gavrusev.dev>
2025-05-17 18:18:26 +0300

    feat: init codecompanion

---
+++ nvim/lua/plugins/ai.lua
@@ -0,0 +1,60 @@
+return {
+	{
+		"nvim-lua/plenary.nvim",
+		lazy = true
+	},
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		ft = { "codecompanion" },
+		opts = {
+			file_types = { "codecompanion" }
+		}
+	},
+	{
+		"olimorris/codecompanion.nvim",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+		},
+		event = "VeryLazy",
+		opts = {
+			adapters = {
+				ollama = function()
+					local openai = require("codecompanion.adapters.openai")
+
+					return require("codecompanion.adapters").extend("ollama", {
+						opts = {
+							tools = true
+						},
+						handlers = {
+							tools = {
+								format_tool_calls = function(self, tools)
+									return openai.handlers.tools.format_tool_calls(self, tools)
+								end,
+								output_response = function(self, tool_call, output)
+									return openai.handlers.tools.output_response(self, tool_call, output)
+								end,
+							},
+						},
+						schema = {
+							model = {
+								default = "qwen3:14b",
+							},
+						},
+					})
+				end,
+			},
+			strategies = {
+				chat = {
+					adapter = "ollama",
+				},
+				inline = {
+					adapter = "ollama",
+				},
+				cmd = {
+					adapter = "ollama",
+
+				},
+			},
+		},
+	},
+}
--- nvim/lua/plugins/lang/markdown.lua
+++ nvim/lua/plugins/lang/markdown.lua
@@ -23,5 +23,13 @@ return {
 				"markdown"
 			})
 		end,
-	}
+	},
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		ft = { "markdown" },
+		opts = {
+			file_types = { "markdown" }
+		},
+		opts_extend = { "file_types" }
+	},
 }
